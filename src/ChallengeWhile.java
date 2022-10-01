a9935451c33fb306be09c10fd627c10a2f56fb1b
Hejociara <hejociarav@gmail.com>
2022-10-01 20:17:16 -0300

    "Feature"

--- src/ChallengeWhile.java
+++ src/ChallengeWhile.java
@@ -8,13 +8,20 @@ public class ChallengeWhile {
         double nota = 0;
         double total = 0;
 
-        while (nota != -1 ){
-            System.out.println("Ingrese una nota: ");
+        while (nota != -1) {
+            System.out.println("Ingrese una nota(o -1 para salir): ");
+            nota = entrada.nextDouble();
+
+            if(nota <= 10 && nota >= 0){
+            total += nota;
+            cantidadNotas++;
+            }else if(nota != -1){
+                System.out.println("Nota inválida!!! ");
+            }
         }
         double promedio = total / cantidadNotas;
         System.out.println("Promedio = " + promedio);
 
         entrada.close();
-
     }
 }
